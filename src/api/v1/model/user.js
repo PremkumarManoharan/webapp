4183ebb118251be166a50fc07022f39f561be5cd
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-02-05 19:19:31 -0500

    typo fix in user model

--- src/api/v1/model/user.js
+++ src/api/v1/model/user.js
@@ -49,13 +49,10 @@ export const db_status = false;
 
 const syncUserModel  = async () => {
   try {
-    // Synchronize the User table (or any other database operations) after successful connection
     await User.sync({ force: true });
     db_status =  true;
-    // Add your code that depends on the database connection here
   } catch (error) {
     console.error('Unable to connect to the database: waiting..Retry in 5sec');
-    // Retry the connection after a delay (e.g., 10 seconds)
     setTimeout(syncUserModel, 5000);
   }
 }
