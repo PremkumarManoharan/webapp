c8e7a6802412fe58e0cfdf9d4cec5e13e9dde263
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-02-13 17:58:48 -0500

    added integ test

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import app from "./src/api/v1/index.js";
 
-app.listen(3000, ()=> { // move port to .env
+const server  = app.listen(3000, ()=> { // move port to .env
     console.log("Server running on port 3000");
 });
+
+export default server; 
