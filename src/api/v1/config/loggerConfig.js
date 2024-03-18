cccab9e4eab9ab564a12e0ed6a599fe541ffbc13
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-03-18 00:24:18 -0400

    issues with log file location fixed

--- src/api/v1/config/loggerConfig.js
+++ src/api/v1/config/loggerConfig.js
@@ -1,11 +1,19 @@
 import winston from 'winston';
 
 const now = new Date();
+
+const logLocation = "";
+if(process.env.NODE_ENV === "test"){
+    logLocation = 'webapp.log'
+}else{
+    logLocation = '/var/log/webapp.log'
+}
+
 export const logger = winston.createLogger({
   format: winston.format.json(),
   levels: winston.config.npm.levels,
   defaultMeta: { time: now.toISOString() },
   transports: [
-    new winston.transports.File({ filename: '/var/log/webapp/webapp.log' }),
+    new winston.transports.File({ filename: logLocation }),
   ],
 });
