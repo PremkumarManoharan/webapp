ab84d43ad22e3b0c76851a7a81560b06f459f39d
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-03-23 13:39:22 -0400

    fixed verify endpoint

--- src/api/v1/routes/user.js
+++ src/api/v1/routes/user.js
@@ -4,23 +4,23 @@ import { basicAuth,checkDB} from "../middleware/authentication.js";
 import { validatePostAttributes, validatePutAttributes } from "../middleware/validation.js";
 const userRouter = express.Router();
 
-
 userRouter.use(userController.allowOnlyMethod);
-userRouter.use(userController.checkQueryParam);
 
 userRouter.route('/').post(
     [
     userController.checkPayload,
+    userController.checkQueryParam,
     checkDB,
     validatePostAttributes(['username', 'password', 'first_name', 'last_name'])
     ],userController.createUser);
 
-userRouter.route('/self').get([checkDB,basicAuth],userController.getUser);
+userRouter.route('/self').get([checkDB,basicAuth,userController.checkQueryParam],userController.getUser);
 
 
 userRouter.route('/self').put(
     [checkDB,
     basicAuth,
+    userController.checkQueryParam,
     validatePutAttributes(['password', 'first_name', 'last_name'])
     ],userController.updateUser);
 
