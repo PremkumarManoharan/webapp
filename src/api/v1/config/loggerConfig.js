137edff5e7126ee44fc841c5b8edea9067cfd17d
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-03-26 11:47:47 -0400

    fixed timestamp issue

--- src/api/v1/config/loggerConfig.js
+++ src/api/v1/config/loggerConfig.js
@@ -9,9 +9,13 @@ if(process.env.NODE_ENV === "test"){
     logLocation = '/tmp/webapp.log'
 }
 export const logger = winston.createLogger({
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DDTHH:mm:ssZ'
+    }),
+    winston.format.json()
+  ),
   level: 'debug',
-  defaultMeta: { time: (now.toISOString()).slice(0, -5) + "Z" }, //removing milli seconds
   transports: [
     new winston.transports.File({ filename: logLocation }),
   ],
